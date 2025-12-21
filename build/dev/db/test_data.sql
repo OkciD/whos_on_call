ff030a05594900a1c6b8fe23d0d4a910452204b4
Yury Baykov <yury.baykov@mail.ru>
2025-12-21 14:51:49 +0300

    create or update device feature

--- build/dev/db/test_data.sql
+++ build/dev/db/test_data.sql
@@ -1,26 +1,42 @@
-INSERT INTO users (id, name, api_key_hash) VALUES (
-    0,
-    "John Doe",
-    "abb45ef89186194a3e3ee700894caeb3b86ced38db1fa3ec7fd0f5e2ff6d9ec1" -- sha256("lolkek")
+BEGIN TRANSACTION;
+
+-- Вставляем пользователя
+INSERT INTO users (name, api_key_hash)
+VALUES (
+    'John Doe',
+    'abb45ef89186194a3e3ee700894caeb3b86ced38db1fa3ec7fd0f5e2ff6d9ec1' -- sha256("lolkek")
 );
 
-INSERT INTO devices (id, name, type, user_id) VALUES (
-    0,
-    "Workstation",
+-- Получаем ID последнего вставленного пользователя
+WITH user_data AS (
+    SELECT last_insert_rowid() AS user_id
+)
+
+-- Вставляем устройство, используя этот ID
+INSERT INTO devices (name, type, user_id)
+VALUES (
+    'Workstation',
     0, -- laptop
-    0
+    (SELECT user_id FROM user_data)
 );
 
-INSERT INTO device_features (id, type, status, device_id) VALUES
+-- Получаем ID устройства
+WITH device_data AS (
+    SELECT last_insert_rowid() AS device_id
+)
+
+-- Вставляем функции устройства
+INSERT INTO device_features (type, status, device_id)
+VALUES
 (
-    0,
     0, -- mic
-    0, -- inactive,
-    0
+    0, -- mic, inactive
+    (SELECT device_id FROM device_data)
 ),
 (
-    1,
     1, -- camera
-    0, -- inactive,
-    0
-);
+    0, -- inactive
+    (SELECT device_id FROM device_data)
+); 
+
+COMMIT;
