9d0ff74318d4c58f674cc3b664fec6b0a02d174e
Yury Baykov <yury.baykov@mail.ru>
2026-09-19 18:08:20 +0300

    eventbus

---
+++ internal/shared/eventbus/eventbus.go
@@ -0,0 +1,70 @@
+package eventbus
+
+import "sync"
+
+type EventType any
+type EventPayload any
+
+type Event struct {
+	Type    EventType
+	Payload EventPayload
+}
+
+type EventChan chan Event
+
+type EventBus interface {
+	Emit(event Event)
+	On(eventType EventType) EventChan
+	Off(eventTypse EventType, ch EventChan)
+}
+
+type eventBus struct {
+	mu          sync.RWMutex
+	subscribers map[EventType][]EventChan
+}
+
+func NewEventBus() EventBus {
+	return &eventBus{
+		subscribers: make(map[EventType][]EventChan),
+	}
+}
+
+func (eb *eventBus) On(eventType EventType) EventChan {
+	eb.mu.Lock()
+	defer eb.mu.Unlock()
+
+	ch := make(EventChan)
+	eb.subscribers[eventType] = append(eb.subscribers[eventType], ch)
+
+	return ch
+}
+
+func (eb *eventBus) Emit(event Event) {
+	eb.mu.RLock()
+	defer eb.mu.RUnlock()
+
+	chans := append([]EventChan{}, eb.subscribers[event.Type]...)
+	go func() {
+		for _, ch := range chans {
+			ch <- event
+		}
+	}()
+}
+
+func (eb *eventBus) Off(eventType EventType, ch EventChan) {
+	eb.mu.Lock()
+	defer eb.mu.Unlock()
+	if subscribers, ok := eb.subscribers[eventType]; ok {
+		for i, subscriber := range subscribers {
+			if ch == subscriber {
+				eb.subscribers[eventType] = append(subscribers[:i], subscribers[i+1:]...)
+				close(ch)
+				//nolint:revive // фолзит
+				for range ch {
+					// Drain the channel
+				}
+				return
+			}
+		}
+	}
+}
---
+++ internal/shared/eventbus/events.go
@@ -0,0 +1,3 @@
+package eventbus
+
+type EventTypeDeviceFeatureUpdated struct{}
